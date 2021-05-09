0e164cfecc8681af719a7279d2d0fd4e922164b2
CharlesP Dong <52944486+cs2420337407@users.noreply.github.com>
2021-05-09 20:37:08 +0800

    Haoch'en Testgroud
    Add animation W left W right Run

---
+++ AI Demo - Copy/Assets/Haochen Test/Script/Controller.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    public Animator _animation;
+    void Awake()
+    {
+        _animation = GetComponent<Animator>();
+
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            _animation.SetTrigger("Run");
+        }
+    }
+}
---
+++ AI Demo - Copy/Assets/Haochen Test/Script/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	float ver = 0;
+	float hor = 0;
+	public float turnspeed = 10;
+
+	// Use this for initialization
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		hor = Input.GetAxis("Horizontal");
+		ver = Input.GetAxis("Vertical");
+
+	}
+	void Rotating(float hor, float ver)
+	{
+		//Get Direction
+		Vector3 dir = new Vector3(hor, 0, ver);
+		//Direct to 4
+		Quaternion quaDir = Quaternion.LookRotation(dir, Vector3.up);
+		//slow trun
+		transform.rotation = Quaternion.Lerp(transform.rotation, quaDir, Time.fixedDeltaTime * turnspeed);
+
+
+
+	}
+
+	void FixedUpdate()
+	{
+
+
+		if (hor != 0 || ver != 0)
+		{
+			//rotaing
+			Rotating(hor, ver);
+
+
+
+		}
+	}
+}
